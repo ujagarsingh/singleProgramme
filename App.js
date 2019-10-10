b4c0fb9f28c32998242b17de45c177c532e538fc
ujagarsingh <ujagar.singh.meena@gmail.com>
2019-10-10 12:01:22 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,47 +1,80 @@
-function dataObj(){
-    var filterData = ['First Test', 'Second Test'];
-    var _update_class = {};
-    let _new_student_data =  _data.student.map((item_s, inx) => {
-      
-            const student_record = {};
-            let _new_subjectData = item_s.subject.map((item_sub, iex) => {
-                
-                    const sub_record = {};
-                    let _new_exams = item_sub.exams.filter((item_e, idx) => {
-                        var _abc = null;
-                        const _filterData = filterData.filter((element)=>{
-                            if(item_e.exam_name == element){
-                                _abc = item_e;
-                            }
-                        })
-                        return _abc;
-                    });
+SELECT YEAR(date), MONTHNAME(date), SUM(credit) 
+FROM income
+GROUP BY YEAR(date), MONTH(date);
 
-                sub_record['id'] = item_sub.id;
-                sub_record['sub_lavel'] = item_sub.sub_lavel;
-                sub_record['sub_name'] = item_sub.sub_name;
-                sub_record['t_mm'] = item_sub.t_mm;
-                sub_record['t_mo'] = item_sub.t_mo;
-                sub_record['exams'] = _new_exams;
+import React, { Component } from 'react';
+import Sidebar from './Sidebar';
+import ListItem from './ListItem'
 
-                return sub_record;
-                });
+class AboutMe extends Component {
+	state = {
+		month: [
+			{ 'id': 101, 'name': 'January' },
+			{ 'id': 102, 'name': 'February' },
+			{ 'id': 103, 'name': 'March' },
+			{ 'id': 104, 'name': 'April' }],
+		dataObj: []
+	}
+	counter(id) {
+		alert(`i am work ${id}`)
+	}
+	render() {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-sm-4">
+						<Sidebar />
+					</div>
+					<div className="col-sm-8 xsy">
+						<h2 >About Me</h2>
+						<ol>
+							{this.state.month.map((item, inx) => {
+								return (
+									<ListItem
+										key={inx}
+										index={inx}
+										item={item}
+										fun={this.counter}
+									/>
+								)
+							})}
+						</ol>
+					</div>
+				</div>
 
-            student_record['admission_number'] = item_s.admission_number;
-            student_record['student_name'] = item_s.student_name;
-            student_record['father_name'] = item_s.father_name;
-            student_record['subject'] = _new_subjectData;
-
-            console.log(student_record);
-        return student_record;
-		});
-        
-        _update_class['id'] = _data.id;
-        _update_class['class_name'] = _data.class_name;
-        _update_class['class_name_portal'] = _data.class_name_portal;
-        _update_class['checked'] = _data.checked;
-        
-        _update_class['student'] = _new_student_data;
-
-    console.log(_update_class);
+			</div>
+		);
+	}
 }
+
+export default AboutMe;
+
+
+import React, { Component } from 'react';
+class ListItem extends Component {
+    state = {
+        index: '',
+        item: ''
+    }
+    componentDidMount() {
+        this.setState({
+            index: this.props.index,
+            item: this.props.item,
+        })
+    }
+    counter(id) {
+        this.props.fun(id)
+    }
+    render() {
+        const mystyle = { 'page-break-inside': 'avoid' }
+        return (
+            <li style={mystyle} key={this.state.index}>{this.state.item.name}<br />
+                <p>Lorem Ipsum </p>
+                <button className="btn btn-primary" onClick={event => this.counter(this.state.item.id)}>Click</button>
+                <br />
+            </li>
+        );
+    }
+}
+
+export default ListItem;
