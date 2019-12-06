87c1b470cde3fb7c494731bd777550f7c7862b56
ujagarsingh <ujagar.singh.meena@gmail.com>
2019-12-06 19:06:10 +0530

    Update App.js

--- App.js
+++ App.js
@@ -21,3 +21,25 @@ INSERT INTO beautiful (name, age)
 				sub_id = VALUES(sub_id),
 				admission_number = VALUES(admission_number),
 				marks_obtain = VALUES(marks_obtain)";
+
+
+//select * from `test`;
+
+//ALTER TABLE `test` ADD UNIQUE `unique_id`(`emp_id`, `comp_id`, `mnth_id`);
+
+INSERT INTO `test` (emp_id, comp_id, mnth_id, amount)
+    VALUES
+		(1,5,7,1000)
+	ON DUPLICATE KEY UPDATE
+		emp_id = VALUES(emp_id),
+        comp_id = VALUES(comp_id),
+        mnth_id = VALUES(mnth_id),
+        amount = VALUES(amount);
+        
+INSERT INTO `test` (emp_id, comp_id, mnth_id, amount)
+    VALUES
+		(1,4,8,1045),
+		(1,4,8,1045)
+	ON DUPLICATE KEY UPDATE amount = VALUES(amount);
+        
+select * from `test`;
