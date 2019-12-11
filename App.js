219805e867401fe11ced6607d5ec6e1f920ad441
ujagarsingh <ujagar.singh.meena@gmail.com>
2019-12-11 16:49:15 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,45 +1,503 @@
-INSERT INTO beautiful (name, age)
-    VALUES
-		('Helen', 24),
-		('Katrina', 21),
-		('Samia', 22),
-		('Hui Ling', 25),
-		('Yumie', 29)
-	ON DUPLICATE KEY UPDATE
-		age = VALUES(age),
-		 ...
-		-----------------------------------------
-	$query = "INSERT INTO 
-		" . $this->table_name . " 
-			(class_id, exam_id, sub_id, marks_obtain, admission_number) VALUES "; //Prequery
-			
-	$qPart = array_fill(0, count($this->student_obj), "(?, ?, ?, ?, ?)");
-	$query .=  implode(",",$qPart);
-	$query .= "ON DUPLICATE KEY UPDATE
-				class_id = VALUES(class_id), 
-				exam_id = VALUES(exam_id), 
-				sub_id = VALUES(sub_id),
-				admission_number = VALUES(admission_number),
-				marks_obtain = VALUES(marks_obtain)";
-
-
-//select * from `test`;
-
-//ALTER TABLE `test` ADD UNIQUE `unique_id`(`emp_id`, `comp_id`, `mnth_id`);
-
-INSERT INTO `test` (emp_id, comp_id, mnth_id, amount)
-    VALUES
-		(1,5,7,1000)
-	ON DUPLICATE KEY UPDATE
-		emp_id = VALUES(emp_id),
-        comp_id = VALUES(comp_id),
-        mnth_id = VALUES(mnth_id),
-        amount = VALUES(amount);
-        
-INSERT INTO `test` (emp_id, comp_id, mnth_id, amount)
-    VALUES
-		(1,4,8,1045),
-		(1,4,8,1045)
-	ON DUPLICATE KEY UPDATE amount = VALUES(amount);
-        
-select * from `test`;
+{
+  "type": [
+    {
+      "image": "/assets/images/dp.jpg",
+      "name": "Admin",
+      "children": [
+        {
+          "children": [
+            
+          ],
+          "icon": "fas fa-suitcase",
+          "name": "Dashboard",
+          "link": "/dashboard.jsp"
+        },
+        {
+          "children": [
+            
+          ],
+          "icon": "fas fa-umbrella-beach",
+          "name": "Holiday Calendar",
+          "link": "/holiday_calendar.jsp"
+        },
+        {
+          "children": [
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "Portal Data",
+              "link": "/portal_students.jsp"
+            },
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "Software Data",
+              "link": "/all_students.jsp"
+            }
+          ],
+          "icon": "fas fa-user-graduate",
+          "name": "Students",
+          "link": ""
+        },
+        {
+          "children": [
+            
+          ],
+          "icon": "fas fa-user-tie",
+          "name": "Professional",
+          "link": "/all_professionals.jsp"
+        },
+        {
+          "children": [
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "Fees Collection",
+              "link": "/fees_collection.jsp"
+            },
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "Get Fees",
+              "link": "/get_fees.jsp"
+            },
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "Fee Receipt",
+              "link": "/fees_receipt.jsp"
+            }
+          ],
+          "icon": "fas fa-receipt",
+          "name": "Fees/ Transpoart",
+          "link": ""
+        },
+        {
+          "children": [
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "Payment List ",
+              "link": "/payment_list.jsp"
+            },
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "New Payment",
+              "link": "/new_payment.jsp"
+            }
+          ],
+          "icon": "fab fa-gripfire",
+          "name": "Payment",
+          "link": ""
+        },
+        {
+          "children": [
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "Income List ",
+              "link": "/income_list.jsp"
+            },
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "New Income",
+              "link": "/new_income.jsp"
+            }
+          ],
+          "icon": "fas fa-seedling",
+          "name": "Income",
+          "link": ""
+        },
+        {
+          "children": [
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "Marks Class wise",
+              "link": "/all_marks.jsp"
+            },
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "Add Marks Subject wise",
+              "link": "/add_marks.jsp"
+            },
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "Excel to Update",
+              "link": "/excel_to_update.jsp"
+            },
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "Report Card",
+              "link": "/get_marks_sheet.jsp"
+            }
+          ],
+          "icon": "fab fa-buffer",
+          "name": "Report Card",
+          "link": ""
+        },
+        {
+          "children": [
+            {
+              "children": [
+                {
+                  "children": [
+                    {
+                      "children": [
+                        
+                      ],
+                      "icon": "fas fa-suitcase",
+                      "name": "Biography",
+                      "link": "/school_biography.jsp"
+                    },
+                    {
+                      "children": [
+                        
+                      ],
+                      "icon": "fas fa-suitcase",
+                      "name": "Update Info",
+                      "link": "/update_school_info.jsp"
+                    }
+                  ],
+                  "icon": "fas fa-graduation-cap",
+                  "name": "School",
+                  "link": ""
+                },
+                {
+                  "children": [
+                    
+                  ],
+                  "icon": "fa fa-caret-right",
+                  "name": "Main Slider",
+                  "link": "/all_slider.jsp"
+                },
+                {
+                  "children": [
+                    
+                  ],
+                  "icon": "fas fa-suitcase",
+                  "name": "Main Ariticle",
+                  "link": "/all_article.jsp"
+                }
+              ],
+              "icon": "fas fa-suitcase",
+              "name": "Main Website",
+              "link": ""
+            },
+            {
+              "children": [
+                {
+                  "children": [
+                    
+                  ],
+                  "icon": "fas fa-suitcase",
+                  "name": "All Galleries",
+                  "link": "/all_galleries.jsp"
+                },
+                {
+                  "children": [
+                    
+                  ],
+                  "icon": "fas fa-suitcase",
+                  "name": "All Images",
+                  "link": "/all_images.jsp"
+                }
+              ],
+              "icon": "fas fa-suitcase",
+              "name": "Image Gallery",
+              "link": ""
+            },
+            {
+              "children": [
+                
+              ],
+              "icon": "fas fa-suitcase",
+              "name": "Upload Excel",
+              "link": "/upload_excel.jsp"
+            },
+            {
+              "children": [
+                {
+                  "children": [
+                    {
+                      "children": [
+                        
+                      ],
+                      "icon": "fa fa-caret-right",
+                      "name": "Category",
+                      "link": "/all_type_n_time.jsp"
+                    },
+                    {
+                      "children": [
+                        
+                      ],
+                      "icon": "fa fa-caret-right",
+                      "name": "Structrue",
+                      "link": "/all_fee_structure.jsp"
+                    }
+                  ],
+                  "icon": "fas fa-briefcase",
+                  "name": "Fee",
+                  "link": ""
+                },
+                {
+                  "children": [
+                    
+                  ],
+                  "icon": "fa fa-caret-right",
+                  "name": "Transpoart",
+                  "link": "/all_transpoart_root.jsp"
+                }
+              ],
+              "icon": "fab fa-gripfire",
+              "name": "Payment",
+              "link": ""
+            },
+            {
+              "children": [
+                
+              ],
+              "icon": "fas fa-chalkboard-teacher",
+              "name": "Class",
+              "link": "/all_class.jsp"
+            },
+            {
+              "children": [
+                
+              ],
+              "icon": "fas fa-book",
+              "name": "Subject",
+              "link": "/all_subject.jsp"
+            },
+            {
+              "children": [
+                {
+                  "children": [
+                    
+                  ],
+                  "icon": "fa fa-caret-right",
+                  "name": "All Exams",
+                  "link": "/all_exam.jsp"
+                },
+                {
+                  "children": [
+                    
+                  ],
+                  "icon": "fa fa-caret-right",
+                  "name": "Max Marks",
+                  "link": "/subject_max_marks.jsp"
+                }
+              ],
+              "icon": "fab fa-buffer",
+              "name": "Exams",
+              "link": ""
+            },
+            {
+              "children": [
+                
+              ],
+              "icon": "far fa-calendar-alt",
+              "name": "Events / Holiday",
+              "link": "/all_events.jsp"
+            },
+            {
+              "children": [
+                
+              ],
+              "icon": "fa fa-user",
+              "name": "Users",
+              "link": "/all_users.jsp"
+            }
+          ],
+          "icon": "fa fa-cog",
+          "name": "Settings",
+          "link": ""
+        }
+      ]
+    },
+    {
+      "image": "/assets/images/dp.jpg",
+      "name": "Staff",
+      "children": [
+        {
+          "children": [
+            
+          ],
+          "icon": "fas fa-suitcase",
+          "name": "Dashboard",
+          "link": "/dashboard.jsp"
+        },
+        {
+          "children": [
+            
+          ],
+          "icon": "fas fa-umbrella-beach",
+          "name": "Holiday Calendar",
+          "link": "/holiday_calendar.jsp"
+        },
+        {
+          "children": [
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "Portal Data",
+              "link": "/portal_students.jsp"
+            },
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "Software Data",
+              "link": "/all_students.jsp"
+            }
+          ],
+          "icon": "fas fa-user-graduate",
+          "name": "Students",
+          "link": ""
+        },
+        {
+          "children": [
+            
+          ],
+          "icon": "fas fa-suitcase",
+          "name": "Professional",
+          "link": "/view_professional.jsp"
+        },
+        {
+          "children": [
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "Fees Collection",
+              "link": "/fees_collection.jsp"
+            },
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "Get Fees",
+              "link": "/get_fees.jsp"
+            },
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "Fee Receipt",
+              "link": "/fees_receipt.jsp"
+            }
+          ],
+          "icon": "fas fa-receipt",
+          "name": "Fees/ Transpoart",
+          "link": ""
+        },
+        {
+          "children": [
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "Marks Class wise",
+              "link": "/all_marks.jsp"
+            },
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "Add Marks Subject wise",
+              "link": "/add_marks.jsp"
+            },
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "Excel to Update",
+              "link": "/excel_to_update.jsp"
+            },
+            {
+              "children": [
+                
+              ],
+              "icon": "",
+              "name": "Report Card",
+              "link": "/get_marks_sheet.jsp"
+            }
+          ],
+          "icon": "fab fa-buffer",
+          "name": "Report Card",
+          "link": ""
+        }
+      ]
+    },
+    {
+      "image": "/assets/images/dp.jpg",
+      "name": "Student",
+      "children": [
+        {
+          "children": [
+            
+          ],
+          "icon": "fas fa-suitcase",
+          "name": "Dashboard",
+          "link": "/dashboard.jsp"
+        },
+        {
+          "children": [
+            
+          ],
+          "icon": "fas fa-umbrella-beach",
+          "name": "Holiday Calendar",
+          "link": "/holiday_calendar.jsp"
+        },
+        {
+          "children": [
+            
+          ],
+          "icon": "fas fa-user-graduate",
+          "name": "Students",
+          "link": "/view_student.jsp"
+        }
+      ]
+    }
+  ]
+}
